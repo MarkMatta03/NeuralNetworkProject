1401f8ab4f092070c74f619825f4ecfd3ee6a941
Mark Matta <markmatta50@gmail.com>
2025-12-06 01:44:37 +0200

    part1

--- lib/losses.py
+++ lib/losses.py
@@ -1,21 +1,10 @@
 import numpy as np
 
 class MSE:
-    def forward(self, y_true, y_pred):
+    def forward(self, y_true, y_pred): # Compute mean squared error
+
         return np.mean((y_true - y_pred) ** 2)
 
-    def backward(self, y_true, y_pred):
+    def backward(self, y_true, y_pred):   # Gradient of MSE w.r.t predictions
+
         return 2 * (y_pred - y_true) / y_true.size
-
-
-class BCE:
-    def forward(self, y_true, y_pred):
-        eps = 1e-10
-        return -np.mean(
-            y_true * np.log(y_pred + eps) +
-            (1 - y_true) * np.log(1 - y_pred + eps)
-        )
-
-    def backward(self, y_true, y_pred):
-        eps = 1e-10
-        return (y_pred - y_true) / ((y_pred * (1 - y_pred)) + eps)
