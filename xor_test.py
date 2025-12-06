c9dd3b34c50473557d5d9e6b4b535b41c575d532
Mark Matta <markmatta50@gmail.com>
2025-12-06 15:11:01 +0200

    part 1

--- xor_test.py
+++ xor_test.py
@@ -1,15 +1,21 @@
 
+import sys, os
+sys.path.append(os.path.abspath(".."))
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 from lib.layers import Dense
 from lib.activations import Sigmoid, Tanh
 from lib.losses import MSE
 from lib.network import Sequential
+from lib.optimizer import SGD
 
-# XOR dataset
-X = np.array([[0,0],[0,1],[1,0],[1,1]])
-y = np.array([[0],[1],[1],[0]])
 
-# Create model: 2 input neurons → 4 hidden → 1 output
+# XOR Dataset
+X = np.array([[0,0],[0,1],[1,0],[1,1]], dtype=float)
+y = np.array([[0],[1],[1],[0]], dtype=float)
+
 model = Sequential([
     Dense(2, 4),
     Tanh(),
@@ -19,18 +25,24 @@ model = Sequential([
 
 loss_fn = MSE()
 lr = 1.0
+epochs = 50000
 
-# Training
-for epoch in range(10000):
-    output = model.forward(X)
-    loss = loss_fn.forward(y, output)
+loss_history = []
 
-    grad = loss_fn.backward(y, output)
-    model.backward(grad, lr)
+optimizer = SGD(lr=1.0)
 
-    if epoch % 1000 == 0:
-        print(f"Epoch {epoch}, Loss = {loss}")
+for epoch in range(epochs + 1):
+    out = model.forward(X)
+    loss = loss_fn.forward(y, out)
+    loss_history.append(loss)
 
-# Final Predictions
-print("\nPredictions after training:")
+    grad = loss_fn.backward(y, out)
+    model.backward(grad)
+    # Update weights using optimizer
+    for layer in model.layers:
+        if hasattr(layer, "W"):  # only Dense layers
+            optimizer.step([layer.W, layer.b], [layer.grad_W, layer.grad_b])
+
+print("Predictions:")
 print(model.forward(X))
+print("Final Loss:", loss)
